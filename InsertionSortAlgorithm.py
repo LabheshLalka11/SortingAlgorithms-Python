fc9abb2714e91484da1c56a608f0147bff4f602c
Labhesh Lalka <83308233+LabheshLalka11@users.noreply.github.com>
2021-05-12 23:03:40 +0530

    Create InsertionSortAlgorithm.py

---
+++ InsertionSortAlgorithm.py
@@ -0,0 +1,15 @@
+
+
+
+array = [5,4,2,6,8,-1]
+
+def Insertionsort(array):
+    for i in range(1,len(array)):
+        current_value = array[i]
+        position = i 
+    while(position>0 and current_value<array[position-1]):
+        array[position],array[position-1]=array[position-1],array[position]
+        position = position+1 
+    print("Insertion Sort Technique:",array)
+    
+Insertionsort(array)
